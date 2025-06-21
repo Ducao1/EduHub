d8eec1554c01d74892deac2166f1b6a46e907f2e
HDUC122002 <huynhhducc2002@gmail.com>
2025-06-21 16:36:45 +0700

    commit11

--- web-fe/src/app/components/teacher/teacher-dashboard/list-student/list-student.component.ts
+++ web-fe/src/app/components/teacher/teacher-dashboard/list-student/list-student.component.ts
@@ -3,6 +3,7 @@ import { Component, NgModule } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { EnrollmentService } from '../../../../services/enrollment.service';
 import { TeacherNavBarComponent } from "../../teacher-nav-bar/teacher-nav-bar.component";
+import { User } from '../../../../interfaces/user';
 
 @Component({
   selector: 'app-list-student',
@@ -17,7 +18,14 @@ import { TeacherNavBarComponent } from "../../teacher-nav-bar/teacher-nav-bar.co
 })
 export class ListStudentComponent {
   classId!: number;
-  students: any[] = [];
+  students: User[] = [];
+  paginatedStudents: User[] = [];
+  
+  currentPage: number = 1;
+  pageSize: number = 10;
+  totalPages: number = 0;
+  visiblePages: number[] = [];
+
   constructor(
     private enrollmentService: EnrollmentService,
     private route: ActivatedRoute
@@ -33,11 +41,44 @@ export class ListStudentComponent {
       next: (response) => {
         debugger
         this.students = response;
+        this.totalPages = Math.ceil(this.students.length / this.pageSize);
+        this.updatePaginatedStudents();
+        this.visiblePages = this.generateVisiblePageArray(this.currentPage, this.totalPages);
       },
       error: (err) => {
         debugger
         alert(`Lỗi khi lấy danh sách sinh viên: ${err.message}`);
       }
     })
+  }
+
+  updatePaginatedStudents(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.paginatedStudents = this.students.slice(startIndex, endIndex);
+  }
+
+  onPageChange(page: number): void {
+    if (page < 1 || page > this.totalPages) {
+      return;
+    }
+    this.currentPage = page;
+    this.updatePaginatedStudents();
+    this.visiblePages = this.generateVisiblePageArray(this.currentPage, this.totalPages);
+  }
+
+  generateVisiblePageArray(currentPage: number, totalPages: number): number[] {
+    const maxVisiblePages = 5;
+    const halfVisiblePages = Math.floor(maxVisiblePages / 2);
+
+    let startPage = Math.max(currentPage - halfVisiblePages, 1);
+    let endPage = Math.min(startPage + maxVisiblePages - 1, totalPages);
+
+    if (endPage - startPage + 1 < maxVisiblePages) {
+      startPage = Math.max(endPage - maxVisiblePages + 1, 1);
+    }
+
+    return new Array(endPage - startPage + 1).fill(0)
+        .map((_, index) => startPage + index);
   }
 }
